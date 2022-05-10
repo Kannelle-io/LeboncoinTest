376edf0bc818a3b42d6b960ae3eccbd57a31173c
Christophe <christophe@kannelle.io>
2022-05-10 16:54:35 +0200

    feat(Album): convert Album to Entity

--- app/src/main/java/com/test/leboncointest/data/models/Album.kt
+++ app/src/main/java/com/test/leboncointest/data/models/Album.kt
@@ -1,6 +1,12 @@
 package com.test.leboncointest.data.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "album")
 data class Album(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val albumId: Int,
     val title: String,
     val url: String,
