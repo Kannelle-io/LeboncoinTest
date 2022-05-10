8447afa66af25d726ff75b0c2564024c37483a6c
Christophe <christophe@kannelle.io>
2022-05-10 12:29:22 +0200

    feat(AlbumListFragment): create AlbumListViewModel using koin module

--- app/src/main/java/com/test/leboncointest/ui/albumlist/AlbumListFragment.kt
+++ app/src/main/java/com/test/leboncointest/ui/albumlist/AlbumListFragment.kt
@@ -1,12 +1,12 @@
 package com.test.leboncointest.ui.albumlist
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.test.leboncointest.R
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AlbumListFragment : Fragment() {
 
@@ -14,19 +14,13 @@ class AlbumListFragment : Fragment() {
         fun newInstance() = AlbumListFragment()
     }
 
-    private lateinit var viewModel: AlbumListViewModel
+    private val viewModel : AlbumListViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.album_list_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AlbumListViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
