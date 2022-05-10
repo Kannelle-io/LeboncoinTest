94564b9c47401dfd6546da7d4ba864d393cb93cb
Christophe <christophe@kannelle.io>
2022-05-10 12:23:53 +0200

    feat(Module): create repositories module

--- app/src/main/java/com/test/leboncointest/di/Modules.kt
+++ app/src/main/java/com/test/leboncointest/di/Modules.kt
@@ -2,8 +2,10 @@ package com.test.leboncointest.di
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.test.leboncointest.data.api.domain.AlbumRepository
 import com.test.leboncointest.data.api.network.NetworkServiceFactory
 import com.test.leboncointest.data.api.network.ServiceFactory
+import com.test.leboncointest.data.repositories.AlbumRepositoryImpl
 import com.test.leboncointest.data.api.services.AlbumServiceProvider
 import com.test.leboncointest.data.api.services.AlbumServiceProviderImpl
 import org.koin.dsl.module
@@ -32,6 +34,13 @@ val apiModule = module {
     single {
         val albumServiceProvider: AlbumServiceProvider = AlbumServiceProviderImpl(get())
         albumServiceProvider
+    }
+}
+
+val repoModule = module {
+    single {
+        val albumRepository: AlbumRepository = AlbumRepositoryImpl(get())
+        albumRepository
     }
 }
 
