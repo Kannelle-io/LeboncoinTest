eb7a566aecb3691f248852b8c8baba8ae4b85b05
Christophe <christophe@kannelle.io>
2022-05-11 09:27:20 +0200

    feat(Album): make Album Parcelable

--- app/src/main/java/com/test/leboncointest/data/models/Album.kt
+++ app/src/main/java/com/test/leboncointest/data/models/Album.kt
@@ -1,9 +1,12 @@
 package com.test.leboncointest.data.models
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
 @Entity(tableName = "album")
+@Parcelize
 data class Album(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
@@ -11,4 +14,4 @@ data class Album(
     val title: String,
     val url: String,
     val thumbnailUrl: String
-)
+) : Parcelable
